e37674dabfa8b7d261ff141ea02cc0032ff5a4c6
frey <frey@thefactoryhka.com>
2024-08-25 20:13:06 -0500

    Estados imperativos

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -8,10 +8,10 @@ function UseState(props){
     const [state, setState] = useState({
         value: '',
         error: false,
-        loading: false
+        loading: false,
+        deleted: false,
+        confirmed: false
     })
-
-
 
 
     useEffect(()=>{
@@ -23,7 +23,8 @@ function UseState(props){
                     setState({
                         ...state,
                         loading: false,
-                        error: false
+                        error: false,
+                        confirmed: true
                     })
                     // setError(true);
                 }else{
@@ -39,41 +40,80 @@ function UseState(props){
         }
     }, 
     [state.loading])
-    return (
-        <div>
-            <h2>Eliminar {props.name}</h2>
-            <p>Ingresa el código de seguridad.</p>
 
-            {(state.error && !state.loading) && (
-                <p>
-                    Código incorrecto.
-                </p>
-            )}
-            
-            {state.loading && (
-                <p>
-                    Cargando....
-                </p>
-            )}
-
-            <input 
-                placeholder="Digita el codigo de segurida" 
-                value={state.value} 
-                onChange={(event)=>{ 
+    if(!state.deleted && !state.confirmed){
+        return (
+            <div>
+                <h2>Eliminar {props.name}</h2>
+                <p>Ingresa el código de seguridad.</p>
+    
+                {(state.error && !state.loading) && (
+                    <p>
+                        Código incorrecto.
+                    </p>
+                )}
+                
+                {state.loading && (
+                    <p>
+                        Cargando....
+                    </p>
+                )}
+    
+                <input 
+                    placeholder="Digita el codigo de segurida" 
+                    value={state.value} 
+                    onChange={(event)=>{ 
+                        setState({
+                            ...state,
+                            value: event.target.value
+                        })
+                    }}
+                    />
+                <button onClick={()=>{
                     setState({
                         ...state,
-                        value: event.target.value
+                        loading: true
                     })
-                }}
-                />
+
+                }}>Comprobar</button>
+            </div>
+        )
+    }else if(!!state.confirmed && !state.deleted){
+        return (
+            <>
+                <p>Confirmar acción</p>
+                <p><strong>Esta seguro que quiere eliminar es State</strong></p>
+                <button onClick={()=>{
+                    setState({
+                        ...state,
+                        deleted: true
+                    })
+                }}>Si, eliminar</button>
+                <button onClick={()=>{
+                    setState({
+                        ...state,
+                        confirmed: false,
+                        value: ''
+                    })
+                }}>Cancelar acción</button>
+            </>
+        )
+    }else{
+       return(
+        <>
+            <p>El Status <strong>{state.value}</strong> se elimino con exito!</p>
             <button onClick={()=>{
-                setState({
-                    ...state,
-                    loading: true
-                })
-            }}>Comprobar</button>
-        </div>
-    )
+                    setState({
+                        ...state,
+                        confirmed: false,
+                        deleted: false,
+                        value: ''
+                    })
+                }}>Resetear Acción</button>
+        </>
+       )
+    }
+   
 }
 
 export {UseState}
